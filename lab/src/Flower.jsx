8ca9c18c043967464eafc268696ee01f1427eaac
just_rob <118245001+JustRob12@users.noreply.github.com>
2024-12-15 11:58:51 +0800

    Goodluck sa exam palangga

--- lab/src/Flower.jsx
+++ lab/src/Flower.jsx
@@ -1,89 +1,97 @@
 import React, { useEffect } from 'react';
-import './Flower.css'; // Import the CSS file
+import './Flower.css';
+import gsap from 'gsap';
 
 const Flower = () => {
   useEffect(() => {
-    const flower = document.querySelector(".flower");
+    const sparklesTl = gsap.timeline({repeat: -1});
 
-    const petalPartMarkup =
-      '<div class="box"><div class="shape"></div></div>';
+    sparklesTl.to('.flower__sparkle:nth-child(3n)', { y: 10, opacity: 0.5, duration: 2})
+    sparklesTl.to('.flower__sparkle:nth-child(3n-1)', { y: -5, opacity: 0.8, duration: 2, delay: -2})
+    sparklesTl.to('.flower__sparkle:nth-child(3n-2)', { y: 5, opacity: 0.3, duration: 2, delay: -2})
+    sparklesTl.to('.flower__sparkle:nth-child(3n)', { y: -10, opacity: 0.8, duration: 2})
+    sparklesTl.to('.flower__sparkle:nth-child(3n-1)', { y: 10, opacity: 0.6, duration: 2, delay: -2})
+    sparklesTl.to('.flower__sparkle:nth-child(3n-2)', { y: 10, opacity: 0.3, duration: 2, delay: -2})
+    sparklesTl.to('.flower__sparkle', { y: 0, opacity: 0.5, duration: 2})
 
-    const maxParts = 20;
-    const maxPetalsDef = 6;
-    const maxPetals = maxPetalsDef;
+    const flowerTl = gsap.timeline({repeat: -1});
 
-    const partsFontStepDef = 20 / maxParts; // Reduced font size step for smaller flower
-    const partsFontStep = partsFontStepDef;
-    const huetStep = 150 / maxParts;
+    flowerTl.to('.flower', {y: 20, duration: 3})
+    flowerTl.to('.flower', {y: 0, duration: 3})
+    flowerTl.to('.flower__head', {rotateX: '70deg', duration: 3, delay: -6});
+    flowerTl.to('.flower__head', {rotateX: '75deg', duration: 3, delay: -3});
 
-    createFlower();
+    const petalsTl = gsap.timeline({repeat: -1});
 
-    function createFlower() {
-      const angle = 360 / maxPetals;
+    petalsTl.to('.flower__petal--back-left', { rotateZ: '-90deg', ease: 'linear', duration: 3})
+    petalsTl.to('.flower__petal--back-right', { rotateZ: '-90deg', ease: 'linear', duration: 3, delay: -3})
+    petalsTl.to('.flower__petal--front-left', { rotateZ: '-90deg', ease: 'linear', duration: 3, delay: -3})
+    petalsTl.to('.flower__petal--front-right', { rotateZ: '-90deg', ease: 'linear', duration: 3, delay: -3})
 
-      for (let i = 0; i < maxPetals; i++) {
-        const petal = createPetal();
-        const currAngle = angle * i + "deg";
-        const transform = `transform: rotateY(${currAngle}) rotateX(-30deg) translateZ(5vmin)`; // Reduced translateZ for smaller size
+    const steamTl = gsap.timeline({repeat: -1});
 
-        petal.setAttribute("style", transform);
-        flower.appendChild(petal);
-      }
-    }
+    steamTl.to('.flower__seeds', {rotateZ: '-360deg', ease: 'linear', duration: 12})
+    steamTl.to('.flower__leaf--first', {rotateY: '-360deg', zIndex: 1, ease: 'linear', duration: 12, delay: -12})
+    steamTl.to('.flower__leaf--second', {rotateY: '360deg', zIndex: 0, ease: 'linear', duration: 12, delay: -12})
+    steamTl.to('.flower__steam-end', {x: '100%', rotateY: '90deg', ease: 'linear', duration: 3, delay: -12})
+    steamTl.from('.flower__steam-end', {x: '-100%', rotateY: '-90deg', ease: 'linear', duration: 3, delay: -3})
 
-    function createPetal() {
-      let box = createBox(null, 0);
+    const leafShadowTl = gsap.timeline({repeat: -1})
 
-      const petal = document.createElement("div");
-      petal.classList.add("petal");
-
-      for (let i = 1; i <= maxParts; i++) {
-        const newBox = createBox(box, i);
-        box = newBox;
-      }
-
-      petal.appendChild(box);
-      return petal;
-    }
-
-    function createBox(box, pos) {
-      let fontSize = partsFontStep * (maxParts - pos) + "vmin"; // Adjusted font size for smaller petals
-      const half = maxParts / 2;
-      let bright = "50";
-
-      if (pos < half + 1) {
-        fontSize = partsFontStep * pos + "vmin";
-      } else {
-        bright = 10 + (40 / half) * (maxParts - pos);
-      }
-
-      const color = `hsl(${huetStep * pos}, 100%, ${bright}%)`;
-
-      const newShape = document.createElement("div");
-      newShape.classList.add("shape");
-
-      const newBox = document.createElement("div");
-      newBox.classList.add("box");
-      newBox.setAttribute("style", `color: ${color}; font-size: ${fontSize}`);
-
-      if (box) {
-        newBox.appendChild(box);
-      }
-
-      newBox.appendChild(newShape);
-      return newBox;
-    }
+    leafShadowTl.fromTo('.flower__leaf--first', {filter: 'brightness(1)'}, {filter: 'brightness(0.5)', duration: 3})
+    leafShadowTl.to('.flower__leaf--first', {filter: 'brightness(1)', duration: 3})
+    leafShadowTl.fromTo('.flower__leaf--second', {filter: 'brightness(1)'}, {filter: 'brightness(0.5)', duration: 3})
+    leafShadowTl.to('.flower__leaf--second', {filter: 'brightness(1)', duration: 3})
   }, []);
 
   return (
-    <div className="wrappers">
-      <div className="flower"></div>
-      {/* Message below the flower */}
-      <div className="message">
-        <h2>I'm so Sorry Palangga</h2>
-    
+    <>
+      <div className="flower__wrapper">
+        <div className="flower__sparkles">
+          <div className="flower__sparkle"></div>
+          <div className="flower__sparkle"></div>
+          <div className="flower__sparkle"></div>
+          <div className="flower__sparkle"></div>
+          <div className="flower__sparkle"></div>
+          <div className="flower__sparkle"></div>
+          <div className="flower__sparkle"></div>
+          <div className="flower__sparkle"></div>
+        </div>
+        <div className="flower">
+          <div className="flower__steam-wrapper">
+            <div className="flower__leaf flower__leaf--first"></div>
+            <div className="flower__leaf flower__leaf--second"></div>
+            <div className="flower__steam">
+              <div className="flower__steam-end"></div>
+            </div>
+          </div>
+          <div className="flower__head flower__head--main">
+            <div className="flower__petal flower__petal--back flower__petal--left flower__petal--back-left"></div>
+            <div className="flower__petal flower__petal--back flower__petal--right flower__petal--back-right"></div>
+            <div className="flower__petal flower__petal--front flower__petal--left flower__petal--front-left"></div>
+            <div className="flower__petal flower__petal--front flower__petal--right flower__petal--front-right"></div>
+            <div className="flower__center">
+              <div className="flower__seeds">
+                <div className="flower__seed"></div>
+                <div className="flower__seed"></div>
+                <div className="flower__seed"></div>
+                <div className="flower__seed"></div>
+                <div className="flower__seed"></div>
+                <div className="flower__seed"></div>
+                <div className="flower__seed"></div>
+                <div className="flower__seed"></div>
+                <div className="flower__seed"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="message-overlay">
+        <div className="message-line">Goodluck sa Exam Palangga</div>
+        <div className="message-line">Kaya nato niiii</div>
+        <div className="message-line">I love you ❤️</div>
+      </div>
+    </>
   );
 };
 
